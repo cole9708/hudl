bb98e44c9f50445d8598e18cd0517950e73ff238
chris cole <cole9708@googlemail.com>
2022-11-17 21:58:45 +0000

    correct spelling of verify

--- tests/selenium_tests/test_login.py
+++ tests/selenium_tests/test_login.py
@@ -17,7 +17,7 @@ class TestHomePage(BaseClass):
         loginPage.get_email_field().send_keys(username)
         loginPage.get_password_field().send_keys(password)
         loginPage.click_login_button()
-        self.verfify_current_url('https://www.hudl.com/home')
+        self.verify_current_url('https://www.hudl.com/home')
         assert self.driver.current_url == 'https://www.hudl.com/home'
         assert homePage.get_logged_in_intials() == 'Chris C'
 
@@ -28,7 +28,7 @@ class TestHomePage(BaseClass):
         loginPage.get_email_field().send_keys(username)
         loginPage.get_password_field().send_keys(wrong_password)
         loginPage.click_login_button()
-        self.verfify_elements_exist(homePage.login_error_banner)
+        self.verify_elements_exist(homePage.login_error_banner)
         assert homePage.get_login_error_banner_text()== "We didn't recognize that email and/or password.Need help?"
 
     def test_unsuccessful_login_no_password(self):
@@ -37,7 +37,7 @@ class TestHomePage(BaseClass):
         homePage.click_sign_in()
         loginPage.get_email_field().send_keys(username)
         loginPage.click_login_button()
-        self.verfify_elements_exist(homePage.login_error_banner)
+        self.verify_elements_exist(homePage.login_error_banner)
         assert homePage.get_login_error_banner_text()== "We didn't recognize that email and/or password.Need help?", "text does not match"
 
     def test_unsuccessful_login_with_no_username(self):
@@ -46,7 +46,7 @@ class TestHomePage(BaseClass):
         homePage.click_sign_in()
         loginPage.get_password_field().send_keys(password)
         loginPage.click_login_button()
-        self.verfify_elements_exist(homePage.login_error_banner)
+        self.verify_elements_exist(homePage.login_error_banner)
         assert homePage.get_login_error_banner_text() == "We didn't recognize that email and/or password.Need help?"
 
     def test_password_reset(self):
@@ -54,9 +54,9 @@ class TestHomePage(BaseClass):
         loginPage = LoginPage(self.driver)
         loginHelpPage = LoginHelpPage(self.driver)
         loginPage.click_need_help_link()
-        self.verfify_current_url('https://www.hudl.com/login/help')
+        self.verify_current_url('https://www.hudl.com/login/help')
         assert self.driver.current_url == 'https://www.hudl.com/login/help'
         assert loginHelpPage.get_reset_email_field_value() == username
         loginHelpPage.click_reset_button()
-        self.verfify_current_url('https://www.hudl.com/login/check-email')
+        self.verify_current_url('https://www.hudl.com/login/check-email')
         assert self.driver.current_url == 'https://www.hudl.com/login/check-email'
--- utilities/BassClass.py
+++ utilities/BassClass.py
@@ -17,15 +17,15 @@ class BaseClass:
         loginPage.get_email_field().send_keys(getData["email"])
         loginPage.get_password_field().send_keys(getData["password"])
         loginPage.click_login_button()
-    def verfify_links_exist(self, text):
+
+    def verify_links_exist(self, text):
         wait = WebDriverWait(self.driver, 10)
         wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, text)))
 
-    def verfify_elements_exist(self, text):
+    def verify_elements_exist(self, text):
         wait = WebDriverWait(self.driver, 10)
         wait.until(expected_conditions.presence_of_element_located((text)))
 
-    def verfify_current_url(self, url):
+    def verify_current_url(self, url):
         wait = WebDriverWait(self.driver, 10)
         wait.until(expected_conditions.url_matches(url))
-
